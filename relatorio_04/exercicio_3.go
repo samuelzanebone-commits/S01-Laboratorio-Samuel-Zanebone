c02deb20536d09e508027cfc5a6184377eacf650
zanebas <samuelzanebone@gmail.com>
2026-09-04 18:24:27 -0300

    Implement plantão schedule generator in Go

--- relatorio_04/exercicio_3.go
+++ relatorio_04/exercicio_3.go
@@ -1,1 +1,27 @@
-nao há enunciado no exercicio 3 do pdf
+package main
+
+import "fmt"
+
+func gerarEscalaPlantao(n int) {
+	fmt.Println("--- Escala de Plantão Técnico ---")
+	
+	dia := 1
+	for j := 1; j <= n; j++ {
+		fmt.Printf("Plantão %d: Dia %d do mês\n", j, dia)
+		dia = dia + 4
+	}
+}
+
+
+
+func main() {
+	var plantoes int
+
+	fmt.Print("Digite a quantidade de plantões necessários: ")
+	fmt.Scanln(&plantoes)
+	fmt.Println()
+
+
+
+	gerarEscalaPlantao(plantoes)
+}
